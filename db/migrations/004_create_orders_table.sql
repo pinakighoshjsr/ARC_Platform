795fa22b34d3f3f05afdb440d92aa410f5e77b7a
Pinaki Ghosh <pinaki.ghosh@nexval.in>
2026-04-28 16:50:47 +0530

    Create the table orders

---
+++ db/migrations/004_create_orders_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS orders 
+(
+    id SERIAL PRIMARY KEY,
+    user_id UUID,
+    amount DECIMAL(10,2) NOT NULL,
+    status VARCHAR(20) DEFAULT 'PENDING',
+    created_at TIMESTAMP DEFAULT NOW()
+);
