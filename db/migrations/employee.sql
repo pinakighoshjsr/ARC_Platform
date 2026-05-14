001d08b78d16e5cb91ee434af196d201a72f2e80
Pinaki Ghosh <pinaki.ghosh@nexval.in>
2026-05-14 14:46:32 +0530

    Create the table employee

---
+++ db/migrations/employee.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS employee
+(
+    employee_id SERIAL PRIMARY KEY,
+    employee_name VARCHAR(100) NOT NULL,
+    department VARCHAR(50),
+    salary NUMERIC(10,2),
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
